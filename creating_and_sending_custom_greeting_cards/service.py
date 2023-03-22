9b27dabca856f5acce65408b498f46481638da3a
JayaniH <jayani.hewa77@gmail.com>
2023-03-22 13:39:59 +0530

    Address review comments

--- creating_and_sending_custom_greeting_cards/service.py
+++ creating_and_sending_custom_greeting_cards/service.py
@@ -10,6 +10,17 @@ from flask import request
 
 from creating_and_sending_custom_greeting_cards import email_client
 
+COMPLETIONS_API_PARAMS = {
+    "temperature": 0.7,
+    "max_tokens": 100,
+    "model": "text-davinci-003",
+}
+
+IMAGE_API_PARAMS = {
+    "n": 1,
+    "size": "1024x1024"
+}
+
 
 @app.route('/send_greeting', methods=['POST'])
 def create_and_send_greeting():
@@ -18,11 +29,11 @@ def create_and_send_greeting():
         occasion = json_body.get('occasion')
         recipient_email = json_body.get('recipient_email')
         email_subject = json_body.get('email_subject')
-        special_notes = json_body.get('special_notes') or ""
+        special_notes = json_body.get('special_notes', "")
 
         if occasion is None or recipient_email is None or email_subject is None:
             logging.error("Request is not properly formatted")
-            return
+            return "Request is not properly formatted", 400
 
         with ThreadPoolExecutor() as executor:
             greeting_prompt = "Generate a greeting for a/an" + occasion + "\nSpecial notes: " + special_notes
@@ -36,14 +47,14 @@ def create_and_send_greeting():
 
             if greeting is None or image_url is None:
                 logging.error("Error generating greeting or design.")
-                return "Error generating greeting or design."
+                return "Error generating greeting or design.", 500
 
             send_email(greeting, image_url, recipient_email, email_subject)
             return "Successfully created and sent greeting."
 
     except Exception as e:
         logging.error("Error creating and sending greeting: " + str(e), exc_info=True)
-        return "Error creating and sending greeting."
+        return "Error creating and sending greeting.", 500
 
 
 def send_email(greeting, image_url, recipient, subject):
@@ -64,14 +75,14 @@ def send_email(greeting, image_url, recipient, subject):
 
     except Exception as e:
         logging.error("Error sending email: " + str(e), exc_info=True)
+        raise e
 
 
 def generate_greeting(prompt):
     try:
         response = openai.Completion.create(
             prompt=prompt,
-            model="text-davinci-003",
-            max_tokens=100
+            **COMPLETIONS_API_PARAMS
         )
         return response["choices"][0]["text"].strip(" \n")
 
@@ -83,8 +94,7 @@ def generate_design(prompt):
     try:
         response = openai.Image.create(
             prompt=prompt,
-            n=1,
-            size="1024x1024"
+            **IMAGE_API_PARAMS
         )
         image_url = response['data'][0]['url']
         return image_url
